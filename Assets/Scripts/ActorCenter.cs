48e48afa5363779504b663b605bdb3294772d46b
nikki <nikkimii@gmail.com>
2021-04-17 16:04:09 +0800

    設定UI , arduino 串接

---
+++ Assets/Scripts/ActorCenter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorCenter : HimeLib.SingletonMono<ActorCenter>
+{
+    public ArduinoInteractive arduino;
+    public string RecieveSymbol = "7";
+    public List<ActorFiles> actorFiles;
+    public int curentIndex;
+
+    
+
+    void Start(){
+        arduino.OnRecieveData += SwitchActor;
+    }
+
+    public void SwitchActor(string x){
+        if(x != RecieveSymbol)
+            return;
+
+        Debug.Log("Switch!");
+    }
+}
